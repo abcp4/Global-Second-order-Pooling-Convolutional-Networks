a69d735aa07708731ed2bb5da7f0bea8ba73ddf1
Ivan Pereira <navi1921@gmail.com>
2020-01-22 02:15:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -224,8 +224,8 @@ def main():
     # Data loading code
     traindir = os.path.join(args.data, 'train')
     valdir = os.path.join(args.data, 'val')
-    #normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-    #                                 std=[0.229, 0.224, 0.225]) 
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225]) 
     
 
     train_dataset = datasets.ImageFolder(
