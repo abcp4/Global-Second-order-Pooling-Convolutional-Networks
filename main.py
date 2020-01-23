80b8792c340c6c626f68e922559b821f38cf2813
Ivan Pereira <navi1921@gmail.com>
2020-01-23 17:37:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -241,7 +241,7 @@ def main():
     # Data loading code
     traindir = os.path.join(args.data, 'train')
     valdir = os.path.join(args.data, 'val')
-    testvaldir = os.path.join(args.data, 'test')
+    testdir = os.path.join(args.data, 'test')
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225]) 
     
